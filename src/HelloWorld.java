3eb69793bc900e5a852e8386751724cc2820d575
phk72 <phk72@LAPTOP-4P74K0PQ>
2020-03-20 19:44:05 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("¡ß Book Rental Management System ¡ß");
+		System.out.println(" 1. Add Customer");
+		System.out.println(" 2. Delete Customer");
+		System.out.println(" 3. Edit Customer");
+		System.out.println(" 4. View Customer");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one menu between 1 - 5");
+	}
+}
